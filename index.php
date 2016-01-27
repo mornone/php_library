8fb74cfa853d7a517f82a51b535928ef049e3d99
flc <3407725@qq.com>
2016-01-27 10:54:15 +0800

    新增校验方法

--- index.php
+++ index.php
@@ -21,4 +21,23 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Library\Validate;
 
-echo Validate::isInteger('123123');
+//echo Validate::isEmpty('1');
+//echo Validate::isNumeric(1);
+//echo Validate::isString('123123');
+//echo Validate::isInteger('123123');
+//echo Validate::isFloat('123123.00');
+//echo Validate::isEmail('1@1.com');
+//echo Validate::isMobile('17012234567');
+//echo Validate::isUrl('ftp://www.123.com');
+//echo Validate::isIp('1.1.1.1');
+//echo Validate::isJson('{"name":"BeJson","url":"http://www.SoSo.com"}');
+//echo Validate::isAlphaDash('12312--3');
+echo Validate::isAlphaNum('123123');
+//echo Validate::isInteger('123123');
+//echo Validate::isInteger('123123');
+//echo Validate::isInteger('123123');
+//echo Validate::isInteger('123123');
+//echo Validate::isInteger('123123');
+//echo Validate::isInteger('123123');
+//echo Validate::isInteger('123123');
+//echo Validate::isInteger('123123');
